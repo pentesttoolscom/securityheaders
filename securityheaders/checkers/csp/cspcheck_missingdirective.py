28ee32de6ecb39b2b9c659275aab410a9b46142b
Stefan Mihalache <122876880+stefan-mihalache@users.noreply.github.com>
2024-04-16 16:36:14 +0300

    Replace questions in cspcheck_missingdirective.py

--- securityheaders/checkers/csp/cspcheck_missingdirective.py
+++ securityheaders/checkers/csp/cspcheck_missingdirective.py
@@ -18,7 +18,7 @@ class CSPCheckMissingDirective(CSPCheck):
         if csp.parsedstring and csp.directive.DEFAULT_SRC in csp.parsedstring:
             defaultSrcValues = csp[csp.directive.DEFAULT_SRC]
             if not csp.directive.OBJECT_SRC in csp.parsedstring and (not csp.keyword.NONE in defaultSrcValues or not str(csp.keyword.NONE) in defaultSrcValues):
-                findings.append(Finding(csp.headerkey, FindingType.MISSING_DIRECTIVES, 'Can you restrict object-src to \'none\'?',FindingSeverity.HIGH_MAYBE, csp.directive.OBJECT_SRC))
+                findings.append(Finding(csp.headerkey, FindingType.MISSING_DIRECTIVES, 'We recommend restricting object-src to \'none\'.',FindingSeverity.HIGH_MAYBE, csp.directive.OBJECT_SRC))
             if csp.directive.BASE_URI in csp.parsedstring:
                 return findings
             else:
@@ -30,11 +30,11 @@ class CSPCheckMissingDirective(CSPCheck):
             if not csp.parsedstring or not directive in csp.parsedstring:
                 description = directive.value + ' directive is missing.'
                 if directive == csp.directive.OBJECT_SRC:
-                    description = 'Missing object-src allows the injection of plugins which can execute JavaScript. Can you set it to \'none\'?'
+                    description = 'Missing object-src allows the injection of plugins which can execute JavaScript. We recommend setting it to \'none\'.'
                 elif directive == csp.directive.BASE_URI:
                     if not csp.policyHasScriptNonces() and not csp.policyHasScriptHashes() and csp.policyHasStrictDynamic():
                         continue
-                    description = 'Missing base-uri allows the injection of base tags. They can be used to set the base URL for all relative (script) URLs to an attacker controlled domain. Can you set it to \'none\' or \'self\'?'
+                    description = 'Missing base-uri allows the injection of base tags. They can be used to set the base URL for all relative (script) URLs to an attacker controlled domain. We recommend setting it to \'none\' or \'self\'.'
                 findings.append(Finding(csp.headerkey, FindingType.MISSING_DIRECTIVES,description,FindingSeverity.HIGH,directive))
 
         return findings
